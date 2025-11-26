36e78170e3f2ee9b624d5a2e2ad770051133f034
Vignesh J <vigneshjayakumar9221@gmail.com>
2025-11-26 10:23:49 +0530

    Add project files.

---
+++ CartXWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CartXWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
