252a060c071cf2f17ff596b9e44b5d1a864b87b7
Vignesh J <vigneshjayakumar9221@gmail.com>
2025-12-06 17:24:47 +0530

    Implement IRepository and Repository

---
+++ CartX.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CartX.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
