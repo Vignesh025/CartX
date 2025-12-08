52f8be56e3652002dc6d0a05e0073b67852e182c
Vignesh J <vigneshjayakumar9221@gmail.com>
2025-12-08 21:30:44 +0530

    Add Product model and repository

--- CartX.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ CartX.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -7,6 +7,7 @@ namespace CartX.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
         void Save();
     }
 }
---
+++ CartX.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using CartX.DataAccess.Data;
+using CartX.DataAccess.Repository.IRepository;
+using CartX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CartX.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
