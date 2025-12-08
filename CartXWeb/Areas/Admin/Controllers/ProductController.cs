41edcfeb0eb2be1a23ca7bfdd786b2d119cc3b8c
Vignesh J <vigneshjayakumar9221@gmail.com>
2025-12-08 21:30:59 +0530

    Add Product Controller

---
+++ CartXWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using CartX.DataAccess.Data;
+using CartX.DataAccess.Repository.IRepository;
+using CartX.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartXWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitofwork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitofwork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitofwork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitofwork.Product.Add(obj);
+                _unitofwork.Save();
+                TempData["success"] = "Product Created Successfully"; 
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitofwork.Product.Get(u=>u.Id==id);
+            if (productFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitofwork.Product.Update(obj);
+                _unitofwork.Save();
+                TempData["success"] = "Product Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitofwork.Product.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitofwork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            _unitofwork.Product.Remove(obj);
+            _unitofwork.Save();
+            TempData["success"] = "Product Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
