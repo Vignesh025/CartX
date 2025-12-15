f36f8ae6bfd4a9dd4283dad0194671ea172221c2
Vignesh J <vigneshjayakumar9221@gmail.com>
2025-12-15 10:16:08 +0530

    Add Homepage and Edit Margins in other views

--- CartXWeb/Areas/Customer/Controllers/HomeController.cs
+++ CartXWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using CartX.DataAccess.Repository.IRepository;
 using CartX.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,9 +7,22 @@ namespace CartXWeb.Areas.Customer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitofWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitofWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
