b9b5faf66beb1277d0073de8a96119a5bcf2728e
Vignesh J <vigneshjayakumar9221@gmail.com>
2025-12-16 11:27:24 +0530

    Implement IEmailSender

---
+++ CartX.Utilities/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CartX.Utilities
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic to send email
+            return Task.CompletedTask;
+        }
+    }
+}
