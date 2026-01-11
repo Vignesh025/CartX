100b22e67ba007097abdd039c875502509878ce9
Vignesh J <vigneshjayakumar9221@gmail.com>
2026-01-11 14:06:17 +0530

    Make SummaryCartVM more Dynamic

--- CartX.Models/ViewModels/ShoppingCartVM.cs
+++ CartX.Models/ViewModels/ShoppingCartVM.cs
@@ -7,6 +7,6 @@ namespace CartX.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
