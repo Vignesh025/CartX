8adf56d13993e19b52e3e8af2555714db3ac664c
Vignesh J <vigneshjayakumar9221@gmail.com>
2026-01-15 22:37:35 +0530

    Add DbInitializer

---
+++ CartX.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CartX.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
