fdd052ffdc9d48d571934927abb0f629b65a5d8d
Vignesh J <vigneshjayakumar9221@gmail.com>
2026-01-16 20:55:03 +0530

    Implement RoleManagement View

---
+++ CartX.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CartX.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+    }
+}
